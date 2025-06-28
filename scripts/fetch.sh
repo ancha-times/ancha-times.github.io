c424be6d8e22bc9cd095ebe44efd7e188cba794d
Alexey Shpakovsky <alexey@shpakovsky.ru>
2025-06-29 00:56:15 +0200

    fetch on one machine, rebuild html in CI
    do not store html in repo

---
+++ scripts/fetch.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+BIN="$(dirname "$(realpath $0)")"
+TOP="$(dirname "$BIN")"
+DATA="$TOP/data"
+
+cd "$TOP"
+
+git fetch
+git reset --hard origin/main
+
+cd "$DATA"
+
+# delete duplicate description files
+ls *.description | cut -c10-20 | sort | uniq -c | awk '$1=='2' {print $2}' | xargs -I % sh -c 'rm *%*'
+
+# create new da file
+ls *.description | sed -r 's_^[^-]*-(.{11})-.*_youtube \1_' >da.txt.new
+
+# delete last 12 entries, to force their refresh
+head -n -12 da.txt.new >da.txt
+
+set -ex
+
+# download yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp >yt-dlp
+chmod a+x ./yt-dlp
+
+./yt-dlp --ignore-errors --write-auto-sub --sub-lang ru --sub-format srv1 --skip-download --write-description -o '%(upload_date)s-%(id)s-%(title)s.%(ext)s' --download-archive da.txt 'https://www.youtube.com/channel/UCXJYy66gIOEsT04ndBUBFPw' || echo 'ignoring errors...'
+
+# rm channel description
+rm -f NA-UCXJYy66gIOEsT04ndBUBFPw*
+
+rm -f da.txt da.txt.new
+
+rm -f yt-dlp
+
+cd "$TOP"
+
+test "$(git status --porcelain data)" = "" && exit 0 || true
+
+git add data
+git commit -m "$(date +%F) update"
+git push
--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -12,45 +12,16 @@ DOSUBS="$TMP/subs.dos.txt"
 
 mkdir -p "$TMP"
 
-cd "$TOP"
-
-git fetch
-git reset --hard origin/main
-
-cd "$DATA"
-
-# delete duplicate description files
-ls *.description | cut -c10-20 | sort | uniq -c | awk '$1=='2' {print $2}' | xargs -I % sh -c 'rm *%*'
-
-# create new da file
-ls *.description | sed -r 's_^[^-]*-(.{11})-.*_youtube \1_' >da.txt.new
-
-# delete last 12 entries, to force their refresh
-head -n -12 da.txt.new >da.txt
-
-set -ex
-
-# download yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp >yt-dlp
-chmod a+x ./yt-dlp
-
-./yt-dlp --ignore-errors --write-auto-sub --sub-lang ru --sub-format srv1 --skip-download --write-description -o '%(upload_date)s-%(id)s-%(title)s.%(ext)s' --download-archive da.txt 'https://www.youtube.com/channel/UCXJYy66gIOEsT04ndBUBFPw' || echo 'ignoring errors...'
-
-# rm channel description
-rm -f NA-UCXJYy66gIOEsT04ndBUBFPw*
-
-rm -f da.txt da.txt.new
-
-rm -f yt-dlp
-
 cp "$TEMPLATES/subs.txt" "$SUBS"
 
 sed '/HERE/,$d' "$TEMPLATES/times.html" >"$TIMES"
 
-set +x
+set -e
 
 export IFS='
 '
+
+cd "$DATA"
 
 for f in `find -iname '*.description' | sort`; do
 	fn="`echo "$f" | sed -r 's/^..//;s/.description$//'`"
@@ -92,15 +63,3 @@ set -x
 
 sed '1,/HERE/d' "$TEMPLATES/times.html" >>"$TIMES"
 sed '/\r/! s/$/\r/' "$SUBS" >"$DOSUBS"
-
-cd -
-
-test "$(git status --porcelain data html)" = "" && exit 0 || true
-
-
-git add data html
-git commit -m "$(date +%F) update"
-git push
-
-gh release delete subs --cleanup-tag --yes
-gh release create subs "$SUBS" "$DOSUBS" --generate-notes
