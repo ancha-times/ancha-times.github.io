6d3c6e6fe3126819a8b50355008a408c9a4ef558
Alexey Shpakovsky <alexey@shpakovsky.ru>
2025-07-02 15:25:05 +0200

    fix old timestamps, one day at a time

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -17,11 +17,30 @@ ls *.description | sed -r 's_^.{8}-.{10}-(.{11})-.*_youtube \1_' >da.txt.new
 # delete last 12 entries, to force their refresh
 head -n -12 da.txt.new >da.txt
 
+# fix timestamps, one day at a time
+bad_date="$(ls *.description | cut -c1-20 | sort | uniq -c | awk '$1!='1' {print $2}' | head -n1)"
+
 set -ex
 
 # download yt-dlp
 test -f yt-dlp || curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp >yt-dlp
 chmod a+x ./yt-dlp
+
+# fix timestamps, one day at a time
+if test -n "$bad_date"; then
+	bad_ids="$(ls "$bad_date"*.description | cut -c21-31)"
+	ts_id_pairs="$(yt-dlp -U --get-filename -o '%(timestamp)s %(id)s' $bad_ids || true)"
+	IFS='
+	'
+	for ts_id in $ts_id_pairs; do
+		ts="${ts_id%% *}"
+		id="${ts_id##* }"
+		echo "[$ts] - [$id]"
+		for f in `ls *$id*`; do
+			mv "$f" "`echo "$f" | cut --output-delimiter=-$ts- -c1-8,21-`"
+		done
+	done
+fi
 
 ./yt-dlp -U --ignore-errors --write-auto-sub --sub-lang ru --sub-format srv1 --skip-download --write-description --write-comments --extractor-args "youtube:max_comments=all,2,100,all" -o '%(upload_date)s-%(timestamp)s-%(id)s-v-%(title)s.%(ext)s' --download-archive da.txt 'https://www.youtube.com/channel/UCXJYy66gIOEsT04ndBUBFPw/videos' || echo 'ignoring errors...'
 ./yt-dlp    --ignore-errors --write-auto-sub --sub-lang ru --sub-format srv1 --skip-download --write-description --write-comments --extractor-args "youtube:max_comments=all,2,100,all" -o '%(upload_date)s-%(timestamp)s-%(id)s-l-%(title)s.%(ext)s' --download-archive da.txt 'https://www.youtube.com/channel/UCXJYy66gIOEsT04ndBUBFPw/streams' || echo 'ignoring errors...'
